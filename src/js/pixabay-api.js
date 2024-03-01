2d2ef88f5c8704cd643a1eef9e010d069e1b2c92
AnnaKupriienko <kupriienko16@gmail.com>
2024-03-01 20:30:07 +0000

    http task

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,1 +1,11 @@
 
+function SearchImages(value) {
+    const params = new URLSearchParams({
+        key: KEY,
+        q: value,
+        image_type: "photo",
+        orientation: "horizontal",
+        safesearch: true,
+   
+    })
+}
---
+++ src/main.js
@@ -0,0 +1,10 @@
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+
+const form = document.querySelector(".search-form")
+const galleryContainer = document.querySelector(".gallery")
+const KEY = "42651463-7e9bc4d6a898bed570bd4622e"
+const BaseUrl = "https://pixabay.com/api/"
