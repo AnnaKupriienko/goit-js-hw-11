0cc299ff2c89a2353365ad3e8e69c806cc002301
AnnaKupriienko <kupriienko16@gmail.com>
2024-03-03 20:00:27 +0000

    1

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,11 +1,43 @@
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
 
-function SearchImages(value) {
+const API_KEY = "42651463-7e9bc4d6a898bed570bd4622e";
+const BaseUrl = "https://pixabay.com/api/";
+const loader = document.querySelector('.loader');
+// let query = "";
+
+export function searchImages(query) {
     const params = new URLSearchParams({
-        key: KEY,
-        q: value,
+        key: API_KEY,
+        q: query,
         image_type: "photo",
         orientation: "horizontal",
         safesearch: true,
-   
     })
+    loader.style.display = 'block';
+
+    return fetch(`${BaseUrl}?${params}`)
+        .then(response => {
+        if (!response.ok) {
+            throw new Error ('Network response was not OK');
+        }
+        return response.json();
+    })
+        .then(data => {
+            loader.style.display = 'none';
+        if (data.hits.length === 0) {
+            iziToast.error({
+                fontSize: 'large',
+                close: false,
+                position: 'topRight',
+                messageColor: 'white',
+                timeout: 2000,
+                backgroundColor: 'red',
+                message:
+                    'Sorry, there are no images matching your search query. Please try again!',
+            });
+        }
+        return data;
+    })
+        .catch(error => { console.log(error) });
 }
--- src/main.js
+++ src/main.js
@@ -1,10 +1,27 @@
-import iziToast from "izitoast";
-import "izitoast/dist/css/iziToast.min.css";
+
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 
-
 const form = document.querySelector(".search-form")
 const galleryContainer = document.querySelector(".gallery")
-const KEY = "42651463-7e9bc4d6a898bed570bd4622e"
-const BaseUrl = "https://pixabay.com/api/"
+import { searchImages } from "./js/pixabay-api.js";
+import { createMarkup} from "./js/render-functions.js";
+
+const lightbox = new SimpleLightbox('.gallery-link', { 
+    captions: true,
+    captionsData: 'alt',
+    captionDelay: 200,
+});
+let query = "";
+form.addEventListener("submit", onSubmit);
+
+function onSubmit(event) {
+    event.preventDefault();
+    galleryContainer.innerHTML = "";
+    query = event.target.elements.query.value.trim(),
+     searchImages(query).then(data => {
+        const markup = createMarkup(data)
+  galleryContainer.insertAdjacentHTML('beforeend', markup)
+        lightbox.refresh()
+    }) .catch(error => console.log(error))
+}
